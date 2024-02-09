eaf247442a7e7a394a3fef6838b4d8f5f7a7e78b
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 10:05:42 -0300

    Publishing

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace openingHours;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, por gentileza, insira a hora: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Olá, por gentileza, insira os minutos: ");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+
+        if(hours <= 7 && hours <= 23){
+            if((hours >= 7 && minutes <30) || (hours == 23 && minutes > 10))
+            Console.WriteLine("Desculpa, mas a entrada não será permitida!");
+        }
+        else Console.WriteLine("Seja bem-vindo! Entrada permitida!");        
+    }
+}
